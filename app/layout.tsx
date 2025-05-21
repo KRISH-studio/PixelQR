e6f382e973dd8f099a77a986a0ace8eb614cc5b3
KRISH-studio <krishsinghal012@gmail.com>
2025-05-21 13:13:40 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -60,11 +60,8 @@ export default function RootLayout({
         <meta name="google-site-verification"
         content="ad-qcNq_GrrumhwGtiGccvsvrFy2TsdRtVJyqWBiKZk" />
 
-        <script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
-          data-ad-client="ca-pub-XXXXXXXXXXXXXXX"
-        ></script>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5670862530868386"
+     crossorigin="anonymous"></script>
         
       </head>
       <link rel="shortcut icon" href="download.png" type="image/x-icon" />
